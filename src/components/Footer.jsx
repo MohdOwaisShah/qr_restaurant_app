c2acac21c329658c7f0a0882765a3e452b5662c2
[MohdOwaisShah] <[owaisshahh245@gmail.com]>
2025-03-05 16:23:25 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,22 @@ const Footer = () => {
             {/* footer-nav */}
             <div className="footer-nav">
                 {/* home-icon */}
-                <i class="fa-solid fa-house home-icon"></i>
+                <i className="fa-solid fa-house home-icon"></i>
                 <p>Home</p>
             </div>
             <div className="footer-nav">
                 {/* reorder-icon */}
-                <i class="fa-solid fa-repeat"></i>
+                <i className="fa-solid fa-repeat"></i>
                 <p>Reorder</p>
             </div>
             <div className="footer-nav">
                 {/* cart-icon */}
-                <i class="fa-solid fa-cart-shopping"></i>
+                <i className="fa-solid fa-cart-shopping"></i>
                 <p>Cart</p>
             </div>
             <div className="footer-nav">
                 {/* Transaction */}
-                <i class="fa-solid fa-money-bill"></i>
+                <i className="fa-solid fa-money-bill"></i>
                 <p>Transaction</p>
             </div>
         </footer>
--- src/components/small-ui/HomeSelectedProduct.jsx
+++ src/components/small-ui/HomeSelectedProduct.jsx
@@ -17,8 +17,9 @@ const HomeSelectedProduct = ({ props }) => {
   })
 
   return (
-    <div className='Home-selected-product' >{filteredData.map((selectedProduct) => {
-      return <div className="home-selected-product">
+    // Home-selected-product
+    <div className='home-selected-product-container' >{filteredData.map((selectedProduct) => {
+      return <div className="home-selected-product" key={selectedProduct.id}>
       {/* close-n-black-bg */}
       <div className="close-n-black-bg">
         {/* close-icon */}
@@ -135,126 +136,6 @@ const HomeSelectedProduct = ({ props }) => {
       </div>
     </div>
     })}</div>
-
-    // home-selected-product
-    // <div className="home-selected-product">
-    //   {/* close-n-black-bg */}
-    //   <div className="close-n-black-bg">
-    //     {/* close-icon */}
-    //     <IoMdCloseCircle className='close-icon' />
-    //   </div>
-    //   {/* selected-product-main-container */}
-    //   <div className="selected-product-main-container">
-    //     {/* selected-image */}
-    //     <img src="/public/Images/home-product-image/biryani.jpg" alt="selected product image" />
-    //     {/* star-n-best-seller-container */}
-    //     <div className="star-n-best-seller-container">
-    //       {/* selected-product-star-container */}
-    //       <div className="selected-product-star-container">
-    //         <FaStar />
-    //         <FaStar />
-    //         <FaStar />
-    //         <FaStar />
-    //         <FaStarHalfAlt />
-    //         <p className='number-of-rating'>11</p>
-    //       </div>
-    //       {/* best-seller-container */}
-    //       <div className="best-seller-container">
-    //         <p>Best seller</p>
-    //       </div>
-    //     </div>
-    //     {/* selected-product-description */}
-    //     <p className="selected-product-description">
-    //       A fragrant, spiced rice dish cooked with tender meat or vegetables, served with a blend of aromatic spices.
-    //     </p>
-    //   </div>
-    //   {/* selected-product-extra-options */}
-    //   <div className="selected-product-extra-options">
-    //     {/* extra-options-heading */}
-    //     <div className="extra-options-heading">
-    //       <h4>Add On</h4>
-    //       <p>You can choose up to 4 options</p>
-    //     </div>
-    //     {/* extra-options-container */}
-    //     <div className="extra-options-container">
-    //       {/* option 1*/}
-    //       <div className="option">
-    //         {/* option-name */}
-    //         <div className="option-name">
-    //           <img src="/public/Images/food-type/non-veg-icon.png" className='veg-n-non-veg-icon' alt="non veg icon" />
-    //           <p>Classic (Mild Spicy)</p>
-    //         </div>
-    //         {/* price-n-checkbox */}
-    //         <div className="price-n-checkbox">
-    //           <p>₹450</p>
-    //           <input type="checkbox" name="" id="check-box" />
-    //         </div>
-    //       </div>
-    //       {/* option 2*/}
-    //       <div className="option">
-    //         {/* option-name */}
-    //         <div className="option-name">
-    //           <img src="/public/Images/food-type/non-veg-icon.png" className='veg-n-non-veg-icon' alt="non veg icon" />
-    //           <p>Hyderabadi Recipe (Spicy)</p>
-    //         </div>
-    //         {/* price-n-checkbox */}
-    //         <div className="price-n-checkbox">
-    //           <p>₹500</p>
-    //           <input type="checkbox" name="" id="check-box" />
-    //         </div>
-    //       </div>
-    //       {/* option 3*/}
-    //       <div className="option">
-    //         {/* option-name */}
-    //         <div className="option-name">
-    //           <img src="/public/Images/food-type/non-veg-icon.png" className='veg-n-non-veg-icon' alt="non veg icon" />
-    //           <p>Chicken Tikka kathi Roll</p>
-    //         </div>
-    //         {/* price-n-checkbox */}
-    //         <div className="price-n-checkbox">
-    //           <p>₹120</p>
-    //           <input type="checkbox" name="" id="check-box" />
-    //         </div>
-    //       </div>
-    //       {/* option 4*/}
-    //       <div className="option">
-    //         {/* option-name */}
-    //         <div className="option-name">
-    //           <img src="/public/Images/food-type/non-veg-icon.png" className='veg-n-non-veg-icon' alt="non veg icon" />
-    //           <p>Extra Egg(1 piece)</p>
-    //         </div>
-    //         {/* price-n-checkbox */}
-    //         <div className="price-n-checkbox">
-    //           <p>₹40</p>
-    //           <input type="checkbox" name="" id="check-box" />
-    //         </div>
-    //       </div>
-    //       {/* option 5 */}
-    //       <div className="option">
-    //         {/* option-name */}
-    //         <div className="option-name">
-    //           <img src="/public/Images/food-type/veg-icon.png" className='veg-n-non-veg-icon' alt="non veg icon" />
-    //           <p>Extra vegetables</p>
-    //         </div>
-    //         {/* price-n-checkbox */}
-    //         <div className="price-n-checkbox">
-    //           <p>₹20</p>
-    //           <input type="checkbox" name="" id="check-box" />
-    //         </div>
-    //       </div>
-    //     </div>
-    //     {/* main-selected-product-add-to-cart-container */}
-    //     <div className="main-selected-product-add-to-cart-container">
-    //       {/* main-selected-product-increment-n-decrement */}
-    //       <div className="main-selected-product-increment-n-decrement">
-    //         <button className='selected-product-decrement-btn'>-</button>
-    //         <span>1</span>
-    //         <button className='selected-product-increment-btn'>+</button>
-    //       </div>
-    //       <button className='selected-product-add-btn'>Add ₹450</button>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
